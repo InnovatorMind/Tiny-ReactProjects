1d54f44c4f7360418380c0f63719601e884f9d6c
InnovatorMind <tarnado445@gmail.com>
2025-12-29 22:33:09 +0530

    tried meterialUI

---
+++ MeterialUI/basic/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { ButtonUsage } from './components/MaterialUI'
+import OutlinedCard from './components/OutlinedCard'
+import RecipeReviewCard from './components/RecipeReviewCard'
+function App() {
+
+  return (
+    <>
+      <ButtonUsage />
+      <OutlinedCard />
+      <RecipeReviewCard />
+    </>
+  )
+}
+
+export default App
